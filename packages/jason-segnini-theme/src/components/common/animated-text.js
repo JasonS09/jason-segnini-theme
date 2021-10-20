92352090a1ba0c4ebcc6d2833f044dfa5fea03ae
JasonS09 <47617351+JasonS09@users.noreply.github.com>
2021-10-20 09:01:12 -0600

    redesigned animated text

--- packages/jason-segnini-theme/src/components/common/animated-text.js
+++ packages/jason-segnini-theme/src/components/common/animated-text.js
@@ -18,29 +18,23 @@ const AnimatedText = ({
     const isWelcomeReceived = state.theme.isWelcomeReceived
     const [states, setStates] = useState({
         content: text.charAt(0),
-        randChar: '',
-        i: 1,
-        j: 0,
-        animationFinished: false
+        i: 1
     })
     const animationSpeed = useRef(speed)
 
     const writeText = () => {
-        let timeout = 0
         const char = text.charAt(states.i)
         animationSpeed.current = speed
     
         if (isCoverText && text.charAt(states?.i-1) === '.') 
             animationSpeed.current = Math.random() < 0.5 ? speed*10 : speed*20
     
-        timeout = setTimeout(
+        const timeout = setTimeout(
             setStates,
             animationSpeed.current,
             states => ({
                 content: states.content + char,
-                randChar: '',
-                i: states.i + 1,
-                j: 0
+                i: states.i + 1
             })
         )
 
@@ -59,26 +53,10 @@ const AnimatedText = ({
         }   
 
         if (isWelcomeReceived || data.isHome) {
-            if (!states.animationFinished) {
-                let timeout = 0
-
-                if (states.j < animationSpeed.current) {
-                    timeout = setTimeout(
-                        setStates,
-                        speed,
-                        states => ({
-                            ...states, 
-                            randChar: '',
-                            j: animationSpeed.current
-                        })
-                    )
-                }
-                else if (text) timeout = writeText()
-
+            if (!states.animationFinished && text) {
+                const timeout = writeText()
                 return () => clearTimeout(timeout)
             }
-            else if (states.randChar !== '')
-                setStates(states => ({...states, randChar: ''}))
 
             if (!isWelcomeReceived) 
                 setTimeout(actions.theme.welcome, 1200)
@@ -88,7 +66,7 @@ const AnimatedText = ({
     useEffect(() => {
         if (states.i === text.length)
             setStates(states => ({
-                ...states,
+                i: states.i,
                 content: text,
                 animationFinished: true
             }))
@@ -97,11 +75,8 @@ const AnimatedText = ({
     useEffect(() => {
         if (states.animationFinished && reanimate)
             setStates({
-                content: '',
-                randChar: '',
-                i: 0,
-                j: 0,
-                animationFinished: false
+                content: text.charAt(0),
+                i: 1
             })
     }, [reanimate])
 
@@ -114,43 +89,43 @@ const AnimatedText = ({
     return (
         <Switch>
             <h1 when={comp==='h1'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </h1>
             <h2 when={comp==='h2'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </h2>
             <h3 when={comp==='h3'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </h3>
             <h4 when={comp==='h4'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </h4>
             <h5 when={comp==='h5'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </h5>
             <h6 when={comp==='h6'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </h6>
             <p when={comp==='p'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </p>
             <strong when={comp==='strong'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </strong>
             <Link when={comp==='a'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </Link>
             <summary when={comp==='summary'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </summary>
             <text when={comp==='text'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
                 {rest.children}
             </text>
             <tspan when={comp==='tspan'} {...rest}>
-                {states.content + states.randChar}
+                {states.content}
             </tspan>
-            <>{states.content + states.randChar}</>
+            <>{states.content}</>
         </Switch>
     )
 }
